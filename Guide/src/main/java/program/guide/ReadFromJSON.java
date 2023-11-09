ce7d7be6e44ac36b283db63f6ea34c02bfce9b8f
marius199911 <mariuar@hiof.no>
2023-11-09 13:05:06 +0100

    Kode for å lese fra json

--- Guide/src/main/java/program/guide/ReadFromJSON.java
+++ Guide/src/main/java/program/guide/ReadFromJSON.java
@@ -1,4 +1,55 @@
 package program.guide;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class ReadFromJSON {
+
+
+
+    public static JSONArray guideJSON;
+    public static JSONArray turistJSON;
+
+    public ReadFromJSON(){
+        JSONParser parser = new JSONParser();
+
+        String json = null;
+        try {
+            json = new String(Files.readAllBytes(Paths.get("src/main/resources/JSON/Guide.json")));
+        } catch (IOException e) {throw new RuntimeException(e);}
+
+        try {
+            guideJSON = (JSONArray) (parser).parse(json);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            json = new String(Files.readAllBytes(Paths.get("src/main/resources/JSON/User.json")));
+
+        } catch (IOException e) {throw new RuntimeException(e);}
+
+        if(!json.isBlank()){
+            try {
+                turistJSON = (JSONArray) (parser).parse(json);
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }}
+
+
+
+    }
+
+    public static JSONArray getGuideJSON() {
+        return guideJSON;
+    }
+
+    public static JSONArray getUserJSON() {
+        return turistJSON;
+    }
 }
