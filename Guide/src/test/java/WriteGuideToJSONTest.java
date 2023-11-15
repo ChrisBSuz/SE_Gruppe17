58660d220f36b5326eb2630fee74433d3349a77d
Ahmad94Hajjar <ahmadhajjar3@hotmail.com>
2023-11-15 09:13:37 +0100

    Testing WriteGuideToJSONTest

---
+++ Guide/src/test/java/WriteGuideToJSONTest.java
@@ -0,0 +1,17 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
+public class WriteGuideToJSONTest {
+    public JSONArray arr;
+    public JSONParser parser = new JSONParser();
+
+
+    public void writeGuideToJson(){
+
+    }
+
+
+
+}
