2999c989ffc445b62148be2b7aa6830abda098a6
Ahmad94Hajjar <ahmadhajjar3@hotmail.com>
2023-11-15 10:23:25 +0100

    Testing WriteGuideToJSONTest

--- Guide/src/test/java/WriteGuideToJSONTest.java
+++ Guide/src/test/java/WriteGuideToJSONTest.java
@@ -1,17 +1,83 @@
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.Test;
+import program.guide.WriteGuideToJSON;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class WriteGuideToJSONTest {
-    public JSONArray arr;
-    public JSONParser parser = new JSONParser();
+
+    //Denne testen kjører i ReadFromJsonTest();
+    public void WriteGuideToJSON(String location, String date, String time, String price, String description) {
+
+        JSONArray array = new JSONArray();
 
 
-    public void writeGuideToJson(){
+        JSONObject obj = new JSONObject();
+        obj.put("location", location);
+        obj.put("date", date.toString());
+        obj.put("time", time);
+        obj.put("price", price);
+        obj.put("description", description);
+        obj.put("id", 1);
+
+
+        String json = null;
+        try {
+            json = new String(Files.readAllBytes(Paths.get("Guide/src/test/resources/JSON/Guide.json")));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        JSONParser parser = new JSONParser();
+        JSONArray oldArray = null;
+        JSONObject oldObject = new JSONObject();
+        try {
+            if(!json.isBlank()){
+
+                oldArray = (JSONArray) parser.parse(json);
+
+                for(int i = 0; i < oldArray.size(); i++){
+                    // array.add(oldArray.get(i));
+                    oldObject = (JSONObject) parser.parse(oldArray.get(i).toString());
+                    obj.put("id",Integer.parseInt(oldObject.get("id").toString())+1);
+
+                    array.add(oldObject);
+
+
+                }
+
+
+
+            }
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        array.add(obj);
+
+        try (
+                FileWriter file = new FileWriter("Guide/src/test/resources/JSON/Guide.json")) {
+            file.write(array.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
-
-
 }
+
+
+
